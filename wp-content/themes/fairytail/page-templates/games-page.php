d8e043cf7c7117f409a915fe1a5b88a3d32810f3
Rachel Vasquez <rachelrvasquez@gmail.com>
2015-06-20 12:06:41 -0400

    challenge graphics
    new sprite, challenge graphics, styled w/ hovers, need to add tables
    and magic fields in now

--- wp-content/themes/fairytail/page-templates/games-page.php
+++ wp-content/themes/fairytail/page-templates/games-page.php
@@ -16,10 +16,14 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-        
       <div class="gmg-wrap">
-          
-         
+        <button role="button" name="challenge-trigger-one" class="btn-chall btn-chall-one">Challenges of the Warrior</button>
+        <button role="button" name="challenge-trigger-two" class="btn-chall btn-chall-two float-right">Challenges of the Artisan</button>
+        <div class="clear"></div>
+        <button role="button" name="challenge-trigger-three" class="btn-chall btn-chall-three">Challenges of the Gatherer</button>
+        <button role="button" name="challenge-trigger-four" class="btn-chall btn-chall-four float-right">Challenges of the Collector</button>
+        <div class="clear"></div>
+        <button role="button" name="challenge-trigger-five" class="btn-chall btn-chall-five">Challenges of the Past</button>
 		  </div><!--gmg-wrap-->
 		</div><!-- #content -->
 	</div><!-- #primary -->
