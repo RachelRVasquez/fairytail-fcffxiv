98e866eb1ce1bd747c2c198ec255683bc2051099
Rachel Vasquez <rachelrvasquez@gmail.com>
2015-06-21 16:53:28 -0400

    magic fields in progress
    Still need to write if statements depending on what kind of challenge
    it is, was able to pull in the achievement icon/link, should probably
    update this on the achievements page at some point…

--- wp-content/themes/fairytail/page-templates/games-page.php
+++ wp-content/themes/fairytail/page-templates/games-page.php
@@ -27,15 +27,34 @@ get_header(); ?>
               <tr>
                   <th class="mobile-first">Challenge</th>
                   <th class="mobile-second">Description</th>
-                  <th class="mobile-second">Achievement</th>
+                  <th class="mobile-third">Achievement</th>
               </tr>
           </thead>
           <tbody>
-              <td class="mobile-first">Some name here</td>
-              <td class="mobile-second">Gotta do all this stuff man</td>
-              <td class="mobile-second">Get achievement info here? how?</td>
+            <?php
+          // second way to get the fields
+     
+          // get_group will retunr an array of all the groups and the group's fields
+          // the parameter for this function is the group's name
+          $chall_entries = get_group('challenge_entry', $post_id=4602);
+          // to see how the array is formed you can use pr($members);
+          // the way the array is formed is
+          // [group index][field name][field index]
+          // The image fields in the array have one more level where the letter "original" and "thumb" which will show
+          // the original or the thumbnail image
+          foreach($chall_entries as $chall_entry){
+            $ach_id = intval($chall_entry['challenge_entry_achievement_id'][1]);
+            $ach_link = get_the_permalink($ach_id);
+            echo "<tr>";  
+            echo "<td class='mobile-first'>" . $chall_entry['challenge_entry_challenge_name'][1] . "</td>";
+            echo "<td class='mobile-second'>" . $chall_entry['challenge_entry_challenge_desc'][1] . "</td>";
+            echo "<td class='mobile-third'><a href='" . $ach_link . "'>" . get_the_post_thumbnail( $ach_id, 'thumbnail' ) . "</a></td>";
+            echo "</tr>";
+          }
+          ?>
           </tbody>
         </table>
+
 
         <button role="button" name="challenge-trigger-two" class="btn-chall btn-chall-two float-right">Challenges of the Artisan</button>
         <div class="clear"></div>
@@ -43,6 +62,8 @@ get_header(); ?>
         <button role="button" name="challenge-trigger-four" class="btn-chall btn-chall-four float-right">Challenges of the Collector</button>
         <div class="clear"></div>
         <button role="button" name="challenge-trigger-five" class="btn-chall btn-chall-five">Challenges of the Past</button>
+        <button role="button" name="challenge-trigger-six" class="btn-chall btn-chall-six float-right">Challenges of the Cards</button>
+        <div class="clear"></div>
 		  </div><!--gmg-wrap-->
 		</div><!-- #content -->
 	</div><!-- #primary -->
