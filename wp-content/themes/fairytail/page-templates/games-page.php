1a3510c867428538fe9badb6cce504b9df600919
Rachel Vasquez <rachelrvasquez@gmail.com>
2015-06-21 17:14:01 -0400

    if statements
    test with dropdown values and if statements…

--- wp-content/themes/fairytail/page-templates/games-page.php
+++ wp-content/themes/fairytail/page-templates/games-page.php
@@ -12,13 +12,15 @@
  * @since blankSlate 3.5
  */
 
-get_header(); ?>
+get_header(); 
+$chall_entries = get_group('challenge_entry', $post_id=4602);
+?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
       <div class="gmg-wrap">
         <button role="button" name="challenge-trigger-one" class="btn-chall btn-chall-one">Challenges of the Warrior</button>
-        <table class="gmg-table">
+        <table class="gmg-table warrior-table">
           <colgroup>
               <col width=""/>
               <col width=""/>
@@ -31,33 +33,53 @@ get_header(); ?>
               </tr>
           </thead>
           <tbody>
-            <?php
-          // second way to get the fields
-     
-          // get_group will retunr an array of all the groups and the group's fields
-          // the parameter for this function is the group's name
-          $chall_entries = get_group('challenge_entry', $post_id=4602);
-          // to see how the array is formed you can use pr($members);
-          // the way the array is formed is
-          // [group index][field name][field index]
-          // The image fields in the array have one more level where the letter "original" and "thumb" which will show
-          // the original or the thumbnail image
-          foreach($chall_entries as $chall_entry){
-            $ach_id = intval($chall_entry['challenge_entry_achievement_id'][1]);
-            $ach_link = get_the_permalink($ach_id);
-            echo "<tr>";  
-            echo "<td class='mobile-first'>" . $chall_entry['challenge_entry_challenge_name'][1] . "</td>";
-            echo "<td class='mobile-second'>" . $chall_entry['challenge_entry_challenge_desc'][1] . "</td>";
-            echo "<td class='mobile-third'><a href='" . $ach_link . "'>" . get_the_post_thumbnail( $ach_id, 'thumbnail' ) . "</a></td>";
-            echo "</tr>";
-          }
+          <?php
+          foreach($chall_entries as $war_entry): //warrior challenges only
+            if( $war_entry['challenge_entry_challenge_type'][1] == 'Warrior' ):
+              $ach_id = intval($war_entry['challenge_entry_achievement_id'][1]);
+              $ach_link = get_the_permalink($ach_id);
+              echo "<tr>";  
+              echo "<td class='mobile-first'>" . $war_entry['challenge_entry_challenge_name'][1] . "</td>";
+              echo "<td class='mobile-second'>" . $war_entry['challenge_entry_challenge_desc'][1] . "</td>";
+              echo "<td class='mobile-third'><a href='" . $ach_link . "'>" . get_the_post_thumbnail( $ach_id, 'thumbnail' ) . "</a></td>";
+              echo "</tr>";
+            endif;
+          endforeach;
           ?>
           </tbody>
         </table>
 
-
         <button role="button" name="challenge-trigger-two" class="btn-chall btn-chall-two float-right">Challenges of the Artisan</button>
         <div class="clear"></div>
+        <table class="gmg-table artisan-table overflow float-right">
+          <colgroup>
+              <col width=""/>
+              <col width=""/>
+          </colgroup>
+          <thead>
+              <tr>
+                  <th class="mobile-first">Challenge</th>
+                  <th class="mobile-second">Description</th>
+                  <th class="mobile-third">Achievement</th>
+              </tr>
+          </thead>
+          <tbody>
+          <?php
+          foreach($chall_entries as $art_entry): //artisan challenges only
+            if( $art_entry['challenge_entry_challenge_type'][1] == 'Artisan' ):
+              $ach_id = intval($art_entry['challenge_entry_achievement_id'][1]);
+              $ach_link = get_the_permalink($ach_id);
+              echo "<tr>";  
+              echo "<td class='mobile-first'>" . $art_entry['challenge_entry_challenge_name'][1] . "</td>";
+              echo "<td class='mobile-second'>" . $art_entry['challenge_entry_challenge_desc'][1] . "</td>";
+              echo "<td class='mobile-third'><a href='" . $ach_link . "'>" . get_the_post_thumbnail( $ach_id, 'thumbnail' ) . "</a></td>";
+              echo "</tr>";
+            endif;
+          endforeach;
+          ?>
+          </tbody>
+        </table>
+
         <button role="button" name="challenge-trigger-three" class="btn-chall btn-chall-three">Challenges of the Gatherer</button>
         <button role="button" name="challenge-trigger-four" class="btn-chall btn-chall-four float-right">Challenges of the Collector</button>
         <div class="clear"></div>
