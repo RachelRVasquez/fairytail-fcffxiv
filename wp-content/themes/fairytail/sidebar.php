0bf0b9d5bd340926729a750e092dae17384b569f
Rachel Vasquez <you@example.com>
2015-06-25 16:10:29 -0400

    Sidebar partial cont.
    Sidebar partial continued…

---
+++ wp-content/themes/fairytail/sidebar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * If no active widgets in sidebar, let's hide it completely.
+ *
+ * @package WordPress
+ * @subpackage blankSlate
+ * @since blankSlate 3.5
+ */
+?>
+
+	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+		<div id="sidebar" class="widget-area" role="complementary">
+      <?php include( locate_template('/partials/sidebar-top.php') ); ?>
+
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+		</div><!-- #secondary -->
+	<?php endif; ?>
