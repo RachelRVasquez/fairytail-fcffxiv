c1c951d2bb9435c11ca7fd6f1c73a9ea8c985bd2
Rachel Vasquez <rachelrvasquez@gmail.com>
2015-10-11 22:50:31 -0400

    messages fix and ls roster
    removed ls roster from roster page and adjusted background colors on
    messages

--- wp-content/themes/fairytail/page-templates/roster-page.php
+++ wp-content/themes/fairytail/page-templates/roster-page.php
@@ -64,54 +64,6 @@ get_header(); ?>
           </table>
          
 		</div><!--roster-wrap-->
-        
-        <div class="roster-wrap">
-          <a href="javascript:void(0);" id="roster-ls" class="roster-link btn-class text-indent border-radius">Linkshell</a>
-          
-          <table class="roster-table">
-          <colgroup>
-              <col width=""/>
-              <col width=""/>
-              <col width="100"/>
-              <col width="100"/>
-              <col width="150"/>
-          </colgroup>
-          <thead>
-              <tr>
-                  <th class="mobile-first">Name</th>
-                  <th class="mobile-second">Joined</th>
-                  <th class="mobile-third">Primary</th>
-                  <th class="mobile-fourth">Secondary</th>
-                  <th class="mobile-fifth">Grand Company</th>
-              </tr>
-          </thead>
-          <tbody>
-          <?php
-          // second way to get the fields
-     
-          // get_group will retunr an array of all the groups and the group's fields
-          // the parameter for this function is the group's name
-          $lsentries = get_group('roster_member_entry', $post_id=94);
-          // to see how the array is formed you can use pr($members);
-          // the way the array is formed is
-          // [group index][field name][field index]
-          // The image fields in the array have one more level where the letter "original" and "thumb" which will show
-          // the original or the thumbnail image
-          foreach($lsentries as $lsentry){
-			$lodestone = $lsentry['roster_member_lodestone_link'][1];
-            echo "<tr>";  
-            echo "<td class='mobile-first'><a href='$lodestone'>".$lsentry['roster_member_name'][1]."</a></td>";
-            echo "<td class='mobile-second'>".$lsentry['roster_member_join_date'][1]."</td>";
-            echo "<td class='job mobile-third'><span class='".$lsentry['roster_member_primary'][1]." btn-class text-indent margin-auto'>'".$lsentry['roster_member_primary'][1]."'</span></td>";
-            echo "<td class='job mobile-fourth'><span class='".$lsentry['roster_member_secondary'][1]." btn-class text-indent margin-auto'>'".$lsentry['roster_member_secondary'][1]."'</span></td>";
-            echo "<td class='company mobile-fifth'><span class='".$lsentry['roster_member_grand_company'][1]." btn-class text-indent margin-auto'>'".$lsentry['roster_member_grand_company'][1]."'</span></td>";
-            echo "</tr>";
-          }
-          ?>
-          </tbody>
-          </table>
-         
-		</div><!--roster-wrap-->
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
